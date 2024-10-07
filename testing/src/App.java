21f2559e2808203b8000c8f5f18fd0891f9044f4
mramya1512 <mudhunuriramyadevi@gmail.com>
2024-10-07 14:01:31 +0600

    Initial commit

---
+++ testing/src/App.java
@@ -0,0 +1,11 @@
+public class App{
+    public static void main(String[] args) throws Exception {
+        int a = 9;
+        int b = 9;
+        int c = 9;
+        if(a == b)
+        {
+        System.out.println("Numbers are equal.");
+    }
+}
+}
