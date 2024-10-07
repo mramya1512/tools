09dfed4a536bcbbe41ee7f8f2687b8d9fdb6f562
mramya1512 <mudhunuriramyadevi@gmail.com>
2024-10-07 14:23:53 +0600

    added else statement

--- testing/src/App.java
+++ testing/src/App.java
@@ -1,10 +1,13 @@
 public class App{
     public static void main(String[] args) throws Exception {
         int a = 9;
-        int b = 9;
+        int b = 8;
         if(a == b)
         {
         System.out.println("Numbers are equal.");
     }
+    else 
+    {System.out.println("Numbers are not equal.");
+}
 }
 }
