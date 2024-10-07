fe9ebde1e1a5fa35b6c9440539d420ea03c9bfa1
mramya1512 <mudhunuriramyadevi@gmail.com>
2024-10-07 14:16:21 +0600

    removed one line

--- testing/src/App.java
+++ testing/src/App.java
@@ -2,7 +2,6 @@ public class App{
     public static void main(String[] args) throws Exception {
         int a = 9;
         int b = 9;
-        int c = 9;
         if(a == b)
         {
         System.out.println("Numbers are equal.");
