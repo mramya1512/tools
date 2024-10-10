d6ba4c2b7d015fc42dae6ea9c01d5a12af5e41af
mramya1512 <mudhunuriramyadevi@gmail.com>
2024-10-10 11:50:34 +0600

    added comment

--- testing/src/App.java
+++ testing/src/App.java
@@ -2,6 +2,7 @@ public class App{
     public static void main(String[] args) throws Exception {
         int a = 9;
         int b = 8;
+        //added if statement.
         if(a == b)
         {
         System.out.println("Numbers are equal.");
